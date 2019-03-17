f5cb85b4848abb28cc2ae460267a3f6d43a2a2b8
lfedorko <lizavieta.fedorko@gmail.com>
2019-03-17 20:38:29 +0200

    group by pow in list + regex changed

--- computorV1.py
+++ computorV1.py
@@ -1,8 +1,6 @@
 import re
 from sys import argv
 
-
-
 BBLUE = '\033[94m'
 BGREEN = '\033[92m'
 BRED = '\033[91m'
@@ -14,41 +12,55 @@ pattern = re.compile(r'((([+-]?)\d+((\.\d+)?)\*)?)X\^\d+')
 
 
 def msg_error(msg):
-	print(BOLD + BRED+ "ERROR: " + ENDC + msg)
+    print(BOLD + BRED + "ERROR: " + ENDC + msg)
+    exit(1)
 
-class Monomial(object):
-
-    def __init__(self, num=None, power=None):
-        self.num = num
-        self.power = power
-
-
-
-
-def change_sign(equ):
 
 
 def validate(equ):
-	equ = equ.replace(" ", "")
-	left_part, right_part = equ.split('=')
-	if not re.fullmatch(r'[X.^\d+*-]{1,}=[X.^\d+*-]{1,}', equ):
-		msg_error("Incorrect symbol!")
-	left_part, right_part = equ.split('=')
-	
+    equ = equ.replace(" ", "")
+    if not re.fullmatch(r'[X.^\d+*-]{1,}=[X.^\d+*-]{1,}', equ):
+        msg_error("Incorrect symbol!")
+    left_part, right_part = equ.split('=')
+    dict = {}
+    dict = find_monomial(left_part, 'left', dict)
+    find_monomial(right_part, 'right', dict)
 
 
-def find_monomial(equ):
-	match_len = 0
-	for match in pattern.finditer(equ):
-		print(match)
-	if len(equ) == len(match_len)
+def change_sign(coef):
+    if coef[0] == '-':
+        return(float(coef[1:]))
+    else:
+        return -float(coef[0:])
 
+
+
+def find_monomial(equ, word, list):
+    match_len = 0
+    sign = 1
+    if equ == "0":
+        return list
+    else:
+        for match icoef, pow = match.group().split("*X^")n pattern.finditer(equ):
+
+            if word == 'right':
+                sign = -1
+            if pow in list:
+                list[pow] = list[pow] + sign * float(coef)
+            else:
+                list[pow] = sign * float(coef)
+            match_len += len(match[0])
+    if len(equ) != match_len:
+        msg_error("Invalid  " + word + " part of equation")
+    print(list)
+    return list
 
 def reduce(equ):
-	pass
+    pass
+
 
 if __name__ == '__main__':
-	if len(argv) != 2:
-		msg_error("Argument != 2")
-	else:
-		validate(argv[1])
+    if len(argv) != 2:
+        msg_error("Argument != 2")
+    else:
+        validate(argv[1])
